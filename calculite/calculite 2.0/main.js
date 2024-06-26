03c420e4266bfe2f09fe6d18d63873cc76fb10ca
renku33 <kurosakirenku@gmail.com>
2024-06-26 10:12:25 +0200

    store values created

--- calculite/calculite 2.0/main.js
+++ calculite/calculite 2.0/main.js
@@ -7,23 +7,47 @@ var secondOperand = ""
 var domNumbersButtons
 var domOperatorsButtons
 
+// initialize calculite
 
 function initCalculator(){
     domNumbersButtons = document.querySelectorAll(".numbers button")
     domOperatorsButtons = document.querySelectorAll(".operators button")
+    domEqualResetNegativeButtons = document.querySelectorAll(".equal_negative_reset button")
 
     addEventListenersToDomNumberButtons()
+    addEventListenersToDomOperatorButtons()
+    addEventListenersToDomEqualResetNegativeButtons()
 }
+
+// add event listeners to our calculator
 
 function addEventListenersToDomNumberButtons(){
     domNumbersButtons.forEach(numberButton => {
-        numberButton.addEventListener("click", () => storeValue(numberButton.value) )
-    });
+        numberButton.addEventListener("click", () => storeNumberValue(numberButton.value) )
+    })
 }
 
-function storeValue(numberButtonValue){
+function addEventListenersToDomOperatorButtons(){
+    domOperatorsButtons.forEach(operatorButton => {
+        operatorButton.addEventListener("click", () => storeOperatorValue(operatorButton.value) )
+    })
+}
+
+function addEventListenersToDomEqualResetNegativeButtons(){
+    domEqualResetNegativeButtons.forEach(button => {
+        button.addEventListener("click", () => storeEqualResetNegativeValue(button.value) )
+    })
+}
+
+// storing functions
+
+function storeOperatorValue(operatorButtonValue){
+    selectedOperator = operatorButtonValue; 
+}
+
+function storeNumberValue(numberButtonValue){
     let tempValue
-    if(!selectedOperator){
+    if(!selectedOperator){   // falta validar si es resultado o no
         tempValue = String(firstOperand) + numberButtonValue  // se pasa a string para poder concatenar
         firstOperand = parseFloat(tempValue)
     } else {
@@ -31,3 +55,24 @@ function storeValue(numberButtonValue){
         secondOperand = parseFloat(tempValue)
     }
 }
+
+function storeEqualResetNegativeValue(EqualResetNegativeValue){
+    switch(EqualResetNegativeValue){
+        case "=":
+            
+            break
+        case "negative":
+            
+            break
+        case "reset":
+            
+            break
+    }
+}
+
+function resetCalculator(){
+
+}
+
+
+// logica calculite
