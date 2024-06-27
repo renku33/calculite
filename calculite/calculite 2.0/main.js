c2485f4b0cd373506c1a995c0cbe60358f6dec82
Max <Max>
2024-06-27 09:11:07 +0200

    se arreglan movidas

--- calculite/calculite 2.0/main.js
+++ calculite/calculite 2.0/main.js
@@ -1,130 +1,133 @@
-var MAX_DISPLAY_DIGIT_LENGTH = 9
+const MAX_DISPLAY_DIGIT_LENGTH = 9
 
-var selectedOperator = ""
-var firstOperand = ""
-var secondOperand = ""
-var result = ""
-var isCurrentOperandNegative = false
-var domNumbersButtons
-var domOperatorsButtons
-var domZeroDecimalButtons
-
+let selectedOperator = ''
+let firstOperand = ''
+let secondOperand = ''
+let result = ''
+let isCurrentOperandNegative = false
+let domNumbersButtons
+let domOperatorsButtons
+let domZeroDecimalButtons
 // initialize calculite
+function initCalculator () {
+  domNumbersButtons = document.querySelectorAll('.numbers button')
+  domOperatorsButtons = document.querySelectorAll('.operators button')
+  domEqualResetNegativeButtons = document.querySelectorAll('.equal_negative_reset button')
+  domZeroDecimalButtons = document.querySelectorAll('.zero_decimal button')
 
-function initCalculator(){
-    domNumbersButtons = document.querySelectorAll(".numbers button")
-    domOperatorsButtons = document.querySelectorAll(".operators button")
-    domEqualResetNegativeButtons = document.querySelectorAll(".equal_negative_reset button")
-    domZeroDecimalButtons = document.querySelectorAll(".zero_decimal button")
-
-    addEventListenersToDomNumberButtons()
-    addEventListenersToDomOperatorButtons()
-    addEventListenersToDomEqualResetNegativeButtons()
-    addEventListenersToDomZeroDecimalButtons()
-    changeCalculatorStatus("disableButton", "operators")
+  addEventListenersToDomNumberButtons()
+  addEventListenersToDomOperatorButtons()
+  addEventListenersToDomEqualResetNegativeButtons()
+  addEventListenersToDomZeroDecimalButtons()
+  changeCalculatorStatus('disableButton', 'operators')
 }
 
 // add event listeners to our calculator
 
-function addEventListenersToDomNumberButtons(){
-    domNumbersButtons.forEach(numberButton => {
-        numberButton.addEventListener("click", () => storeNumberValue(numberButton.value) )
-    })
+function addEventListenersToDomNumberButtons () {
+  domNumbersButtons.forEach(numberButton => {
+    numberButton.addEventListener('click', () => storeNumberValue(numberButton.value))
+  })
 }
 
-function addEventListenersToDomZeroDecimalButtons(){
-    domZeroDecimalButtons.forEach(button => {
-        button.addEventListener("click", () => storeZeroDecimalValue(button.value) )
-    })
+function addEventListenersToDomZeroDecimalButtons () {
+  domZeroDecimalButtons.forEach(button => {
+    button.addEventListener('click', () => storeZeroDecimalValue(button.value))
+  })
 }
 
-function addEventListenersToDomOperatorButtons(){
-    domOperatorsButtons.forEach(operatorButton => {
-        operatorButton.addEventListener("click", () => storeOperatorValue(operatorButton.value) )
-    })
+function addEventListenersToDomOperatorButtons () {
+  domOperatorsButtons.forEach(operatorButton => {
+    operatorButton.addEventListener('click', () => storeOperatorValue(operatorButton.value))
+  })
 }
 
-function addEventListenersToDomEqualResetNegativeButtons(){
-    domEqualResetNegativeButtons.forEach(button => {
-        button.addEventListener("click", () => EqualResetNegativeFunctions(button.value) )
-    })
+function addEventListenersToDomEqualResetNegativeButtons () {
+  domEqualResetNegativeButtons.forEach(button => {
+    button.addEventListener('click', () => equalResetNegativeFunctions(button.value))
+  })
 }
 
 // storing functions
 
-function storeOperatorValue(operatorButtonValue){
-    selectedOperator = operatorButtonValue; 
-    changeCalculatorStatus("updateDisplay","") // hacer que no se borre el numero sino que se actualice por el segundo (toDo)
-    changeCalculatorStatus("enableButton", "numbers")
-   
+function storeOperatorValue (operatorButtonValue) {
+  selectedOperator = operatorButtonValue
+  changeCalculatorStatus('updateDisplay', '') // hacer que no se borre el numero sino que se actualice por el segundo (toDo)
+  changeCalculatorStatus('enableButton', 'numbers')
+  changeCalculatorStatus('enableButton', 'zero_decimal')
 }
 
-function storeNumberValue(numberButtonValue){
-    let tempStringValue
-    
-    if(!selectedOperator){   
-        tempStringValue = String(firstOperand) + numberButtonValue  // se pasa a string para poder concatenar
-        firstOperand = parseFloat(tempStringValue)
-        changeCalculatorStatus("updateDisplay",firstOperand)
-    } else {
-        tempStringValue = String(secondOperand) + numberButtonValue
+function storeNumberValue (numberButtonValue) {
+  let tempStringValue
+
+  if (!selectedOperator) {
+    tempStringValue = String(firstOperand) + numberButtonValue // se pasa a string para poder concatenar
+    firstOperand = parseFloat(tempStringValue)
+    changeCalculatorStatus('updateDisplay', firstOperand)
+  } else {
+    tempStringValue = String(secondOperand) + numberButtonValue
+    secondOperand = parseFloat(tempStringValue)
+    changeCalculatorStatus('updateDisplay', secondOperand)
+  }
+
+  validateAllowedLenght()
+  changeCalculatorStatus('enableButton', 'operators')
+}
+
+function equalResetNegativeFunctions (equalResetNegativeValue) {
+  switch (equalResetNegativeValue) {
+    case '=':
+      const operationIsPossible = validateOperation()
+      if (operationIsPossible) {
+        result = calculateResult()
+        resetCalculator() // resetea el secondOperator
+        validateAllowedLenght() // exclusivamente para el result
+        firstOperand = result
+        changeCalculatorStatus('updateDisplay', result)
+        changeCalculatorStatus('disableButton', 'numbers')
+        changeCalculatorStatus('disableButton', 'zero_decimal')
+      } else {
+        changeCalculatorStatus('updateDisplay', firstOperand)
+      }
+      break
+    case 'negative':
+      toggleNegativePositive()
+      break
+    case 'reset':
+      resetCalculator()
+      break
+  }
+}
+
+function storeZeroDecimalValue (ZeroDecimalValue) {
+  switch (ZeroDecimalValue) {
+    case '0':
+      let tempStringValue
+      if (!selectedOperator) {
+        if (firstOperand) {
+          tempStringValue = String(firstOperand) + ZeroDecimalValue // se pasa a string para poder concatenar
+          firstOperand = parseFloat(tempStringValue)
+          validateAllowedLenght()
+          changeCalculatorStatus('updateDisplay', firstOperand)
+        }
+      } else {
+        tempStringValue = String(secondOperand) + ZeroDecimalValue // se pasa a string para poder concatenar
         secondOperand = parseFloat(tempStringValue)
-        changeCalculatorStatus("updateDisplay",secondOperand)
-    }
-    
-    validateAllowedLenght()
-    changeCalculatorStatus("enableButton", "operators")
+        validateAllowedLenght()
+        changeCalculatorStatus('updateDisplay', secondOperand)
+      }
+      break
+    case '.':
+
+      break
+  }
 }
 
-
-function EqualResetNegativeFunctions(EqualResetNegativeValue){
-    switch(EqualResetNegativeValue){
-        case "=":
-            let operationIsPossible = validateOperation()
-            if(operationIsPossible){ 
-                result = calculateResult()  
-                resetCalculator()
-                validateAllowedLenght()
-                firstOperand = result
-                changeCalculatorStatus("updateDisplay", result)
-            } else {
-                changeCalculatorStatus("updateDisplay", firstOperand)
-            }
-            break
-        case "negative":
-            toggleNegativePositive()
-            break
-        case "reset":
-            resetCalculator()
-            break
-    }
+function updateCalculatorDisplay (displayValue) {
+  document.getElementById('calculatorDisplay').value = String(displayValue).replace('.', ',')
 }
 
-function storeZeroDecimalValue(ZeroDecimalValue){
-    switch(ZeroDecimalValue){
-        case "0":
-            let tempStringValue
-            if(!selectedOperator){   
-                if(firstOperand){
-                    tempStringValue = String(firstOperand) + ZeroDecimalValue  // se pasa a string para poder concatenar
-                    firstOperand = parseFloat(tempStringValue)
-                    changeCalculatorStatus("updateDisplay",firstOperand)
-                }
-            } else {
-              // falta por hacer 
-            }
-            break
-        case ".":
-            
-            break
-    }
-}
-
-function updateCalculatorDisplay(displayValue){
-    document.getElementById("calculatorDisplay").value = String(displayValue).replace('.', ',')
-}
-
-// main 
+// main
 
 /**
  * Esta funcion hace de puente entre los estados que puede tener la calculadora
@@ -134,122 +137,121 @@ function updateCalculatorDisplay(displayValue){
  * @see disableButton
  * @see enableButton
  */
-function changeCalculatorStatus(optionState, parameterToTransfer){
-    switch(optionState){
-        case "updateDisplay": 
-            updateCalculatorDisplay(parameterToTransfer)
-            break
-        case "disableButton":
-            disableButtons(parameterToTransfer)
-            break
-        case "enableButton":
-            enableButtons(parameterToTransfer)
-            break
-    }
+function changeCalculatorStatus (optionState, parameterToTransfer) {
+  switch (optionState) {
+    case 'updateDisplay':
+      updateCalculatorDisplay(parameterToTransfer)
+      break
+    case 'disableButton':
+      disableButtons(parameterToTransfer)
+      break
+    case 'enableButton':
+      enableButtons(parameterToTransfer)
+      break
+  }
 }
-
 
 // logica calculite
-function disableButtons(buttonToBlock){
-    let buttons = document.querySelectorAll(`.${buttonToBlock} button`);
+function disableButtons (buttonToBlock) {
+  const buttons = document.querySelectorAll(`.${buttonToBlock} button`)
 
-    buttons.forEach(function(button) {
-        button.disabled = true;
-    });
+  buttons.forEach(function (button) {
+    button.disabled = true
+  })
 }
 
-function enableButtons(buttonToBlock){
-    let buttons = document.querySelectorAll(`.${buttonToBlock} button`);
+function enableButtons (buttonToBlock) {
+  const buttons = document.querySelectorAll(`.${buttonToBlock} button`)
 
-    buttons.forEach(function(button) {
-        button.disabled = false;
-    });
+  buttons.forEach(function (button) {
+    button.disabled = false
+  })
 }
 
-function resetCalculator(){
-    selectedOperator = ""
-    firstOperand = ""
-    secondOperand = ""
-    isCurrentOperandNegative = false
-    changeCalculatorStatus("enableButton", "numbers")
-    changeCalculatorStatus("updateDisplay",0)   // se actualiza el display de la calculadora al valor inicial
+function resetCalculator () {
+  selectedOperator = ''
+  firstOperand = ''
+  secondOperand = ''
+  isCurrentOperandNegative = false
+  changeCalculatorStatus('enableButton', 'numbers')
+  changeCalculatorStatus('enableButton', 'zero_decimal')
+  changeCalculatorStatus('updateDisplay', 0) // se actualiza el display de la calculadora al valor inicial
 }
 
-function toggleNegativePositive(){
-    if(!selectedOperator){  
-        firstOperand = firstOperand * (-1)
-        changeCalculatorStatus("updateDisplay",firstOperand)
-    } else {
-        secondOperand = secondOperand * (-1)
-        changeCalculatorStatus("updateDisplay", secondOperand)
+function toggleNegativePositive () {
+  if (!selectedOperator) {
+    firstOperand = firstOperand * (-1)
+    changeCalculatorStatus('updateDisplay', firstOperand)
+  } else {
+    secondOperand = secondOperand * (-1)
+    changeCalculatorStatus('updateDisplay', secondOperand)
+  }
+
+  isCurrentOperandNegative = !isCurrentOperandNegative
+  validateAllowedLenght()
+}
+
+function validateOperation () {
+  if (!secondOperand && firstOperand) {
+    secondOperand = firstOperand
+  }
+  return (firstOperand && selectedOperator) // mirar si las variables estan vacias
+}
+
+function calculateResult () {
+  let result
+
+  switch (selectedOperator) {
+    case '+':
+      result = firstOperand + secondOperand
+      break
+    case '-':
+      result = firstOperand - secondOperand
+      break
+    case '/':
+      if (secondOperand == 0) {
+        result = 'error'
+      } else {
+        result = firstOperand / secondOperand
+      }
+      break
+    case '*':
+      result = firstOperand * secondOperand
+      break
+  }
+  return result
+}
+
+function validateAllowedLenght () {
+  const firstOperandString = String(firstOperand) // se pasa a string para poder medir la largura
+  const secondOperandString = String(secondOperand)
+  const resultString = String(result)
+
+  if (!selectedOperator) {
+    validateOperandLength(firstOperandString)
+  } else {
+    validateOperandLength(secondOperandString)
+  }
+
+  if (resultString.length > MAX_DISPLAY_DIGIT_LENGTH) {
+    result = result.toExponential(2)
+  } else if (resultString.length == MAX_DISPLAY_DIGIT_LENGTH) {
+    changeCalculatorStatus('disableButton', 'numbers')
+    changeCalculatorStatus('disableButton', 'zero_decimal')
+    changeCalculatorStatus('disableButton', 'negative')
+  }
+}
+
+function validateOperandLength (operand) {
+  if (operand.length == MAX_DISPLAY_DIGIT_LENGTH) {
+    changeCalculatorStatus('disableButton', 'numbers')
+    changeCalculatorStatus('disableButton', 'zero_decimal')
+    if (!isCurrentOperandNegative) {
+      changeCalculatorStatus('disableButton', 'negative')
     }
-
-    isCurrentOperandNegative = !isCurrentOperandNegative
-    validateAllowedLenght()
+  } else {
+    changeCalculatorStatus('enableButton', 'zero_decimal')
+    changeCalculatorStatus('enableButton', 'numbers')
+    changeCalculatorStatus('enableButton', 'negative')
+  }
 }
-
-function validateOperation(){
-    if(!secondOperand && firstOperand){
-        secondOperand = firstOperand
-    }
-    return (firstOperand && selectedOperator) // mirar si las variables estan vacias
-}
-
-function calculateResult(){
-    let result
-
-    switch(selectedOperator){
-        case "+":
-            result = firstOperand + secondOperand
-            break
-        case "-":
-            result = firstOperand - secondOperand
-            break
-        case "/":
-            if(secondOperand == 0){
-                result = "error"
-            } else {
-                result = firstOperand / secondOperand
-            }
-            break
-        case "*":
-            result = firstOperand * secondOperand
-            break
-    }
-
-    return result
-}
-
-function validateAllowedLenght(){
-    let firstOperandString = String(firstOperand) // se pasa a string para poder medir la largura
-    let secondOperandString = String(secondOperand)
-    let resultString = String(result)
-
-    if(!selectedOperator){  
-        if(firstOperandString.length == MAX_DISPLAY_DIGIT_LENGTH){ 
-            changeCalculatorStatus("disableButton", "numbers")
-            if(!isCurrentOperandNegative){
-                changeCalculatorStatus("disableButton", "negative")
-            }
-        } else {
-            changeCalculatorStatus("enableButton", "numbers")
-            changeCalculatorStatus("enableButton", "negative")
-        }
-    } else {
-        if(secondOperandString.length == MAX_DISPLAY_DIGIT_LENGTH){
-            changeCalculatorStatus("disableButton", "numbers")
-            if(!isCurrentOperandNegative){
-                changeCalculatorStatus("disableButton", "negative")
-            }
-        } else {
-            changeCalculatorStatus("enableButton", "numbers")
-            changeCalculatorStatus("enableButton", "negative")
-        }
-    }
-
-    if(resultString.length > MAX_DISPLAY_DIGIT_LENGTH){
-        result = result.toExponential(2)
-    }
-}
-
-
