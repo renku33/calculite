d675b7e14b27436a51588ca9c2be8163051b9e00
lucxf <luquiluc2005@gmail.com>
2024-06-28 14:26:16 +0200

    Fail to commit arrastro manoplio
    Trabajo del Viernes toca retocarlo pero que esta de pta madre, hbr cuando me toca tocar un pkito de maquinas

--- calculite/calculite 3.0/main.js
+++ calculite/calculite 3.0/main.js
@@ -1,162 +1,237 @@
-// constantes
+// Constantes
 const MAX_DISPLAY_DIGIT_LENGTH = 9
 
-// variables
+// Variables
 let firstOperand = 0
-let currentOperand = ""
-let operator = ""
+let currentOperand = ''
+let operator = ''
 
-// estados booleanos
-let pendingResult = false
+// Booleans
 let pendingReset = false
-let operatorSelected = false
-let maxDigit = false
-let isNegative = false
-let newNum = false
+let hasResult = false
 
-function initCalculator(){
-    domNumbersButtons = document.querySelectorAll('.numbers button')
-    domOperatorsButtons = document.querySelectorAll('.operators button')
-    domEqualButton = document.getElementById('equal')
-    domResetButton = document.getElementById('reset')
-    domNegativeButton = document.getElementById('toggleNegative')
-    domZeroButton = document.getElementById('0')
-    domDecimalButton = document.getElementById('decimal')
+// Buttons
+let domNumberButtons
+let domOperatorsButtons
+let domEqualButton
+let domResetButton
+let domNegativeButton
+let domZeroButton
+let domDecimalButton
 
-    addEventListeners()
+function initCalculator () {
+  domNumberButtons = document.querySelectorAll('.numbers button')
+  domOperatorsButtons = document.querySelectorAll('.operators button')
+  domEqualButton = document.getElementById('equal')
+  domResetButton = document.getElementById('reset')
+  domNegativeButton = document.getElementById('toggleNegative')
+  domZeroButton = document.getElementById('0')
+  domDecimalButton = document.getElementById('decimal')
+
+  addEventListeners()
 }
 
-function addEventListeners(){
-    domNumbersButtons.forEach(numberButton => {
-        numberButton.addEventListener('click', () => storeNumberValue(numberButton.value))
-    })
+function addEventListeners () {
+  domNumberButtons.forEach(numberButton => {
+    numberButton.addEventListener('click', () => storeNumberValue(numberButton.value))
+  })
 
-    domOperatorsButtons.forEach(operatorButton => {
-        operatorButton.addEventListener('click', () => storeOperatorValue(operatorButton.value))
-    })
+  domOperatorsButtons.forEach(operatorButton => {
+    operatorButton.addEventListener('click', () => storeOperatorValue(operatorButton.value))
+  })
 
-    domZeroButton.addEventListener('click', () => addZero())
+  domZeroButton.addEventListener('click', () => addZero())
 
-    domDecimalButton.addEventListener('click', () => addDecimalSeparator())
+  domDecimalButton.addEventListener('click', () => addDecimalSeparator())
 
-    domEqualButton.addEventListener('click', () => hacerCalculo())
+  domEqualButton.addEventListener('click', () => hacerCalculo())
 
-    domResetButton.addEventListener('click', () => resetCalculator())
+  domResetButton.addEventListener('click', () => resetCalculator())
 
-    domNegativeButton.addEventListener('click', () => toggleNegative())
+  domNegativeButton.addEventListener('click', () => toggleNegative())
 }
 
-function storeNumberValue(numberValue){
+function storeNumberValue (numberValue) {
+  if (pendingReset) {
+    pendingReset = false
+    currentOperand = ''
+  }
+  currentOperand += numberValue
+  updateCalculatorStatus()
+}
+
+function storeOperatorValue (operatorValue) {
+  operator = operatorValue
+  firstOperand = Number(currentOperand)
+  pendingReset = true
+  hasResult = false
+  updateCalculatorStatus()
+}
+
+function addZero () {
+  if (currentOperand.includes('.') || currentOperand != '') {
     if (pendingReset) {
-        pendingReset = false
-        currentOperand = ''
-    }
-    currentOperand += numberValue
-    // newNum = true
-    updateCalculatorStatus()
-}
-
-function storeOperatorValue(operatorValue){
-    operator = operatorValue
-    firstOperand = parseFloat(currentOperand)
-    pendingReset = true
-    // operatorSelected = true
-    updateCalculatorStatus()
-}
-
-function addZero(){
-    if (pendingReset) {
-        pendingReset = false
-        currentOperand = ''
+      pendingReset = false
+      currentOperand = ''
     }
     currentOperand += '0'
-    // newNum = true
-    updateCalculatorStatus()
+  }
+  updateCalculatorStatus()
 }
 
-function addDecimalSeparator(){
-    currentOperand = currentOperand.replace('.', '')
-    currentOperand += '.'
-    // newNum = true
-    updateCalculatorStatus()
+function addDecimalSeparator () {
+  currentOperand = currentOperand.replace('.', '')
+  if (currentOperand == '') {
+    currentOperand += '0'
+  }
+  currentOperand += '.'
+  updateCalculatorStatus()
 }
 
-function hacerCalculo(){
+function hacerCalculo () {
+  if (operator) {
     let result = calculateResult()
     result = formatResult(result)
     currentOperand = result
+    hasResult = true
+    operator = ''
+  } else {
+    resetCalculator()
+  }
 
-    updateCalculatorStatus()
+  updateCalculatorStatus()
 }
 
-function resetCalculator(){
-    currentOperand = ""
-    operator = ""
-    // pendingReset= true
-    updateCalculatorStatus()
+function resetCalculator () {
+  currentOperand = ''
+  operator = ''
+  hasResult = false
+  updateCalculatorStatus()
 }
 
-function toggleNegative(){
-    if(currentOperand.startsWith('-')) {
-        currentOperand.replace('-', '')
+function toggleNegative () {
+  if (Number(currentOperand) != 0) {
+    if (currentOperand.startsWith('-')) {
+      currentOperand = currentOperand.replace('-', '')
     } else {
-        currentOperand = '-' + currentOperand
+      currentOperand = '-' + currentOperand
     }
-
-    updateCalculatorStatus()
+  }
+  updateCalculatorStatus()
 }
 
-function calculateResult() {
-    let result
+function calculateResult () {
+  let result
 
-    switch (operator) {
-        case '+':
-            result = firstOperand + parseFloat(currentOperand)
-            break
-        case '-':
-            result = firstOperand - parseFloat(currentOperand)
-            break
-        case '/':
-            if (parseFloat(currentOperand) == 0) {
-                result = 'error'
-            } else {
-                result = firstOperand / parseFloat(currentOperand)
-            }
-            break
-        case '*':
-            result = firstOperand * parseFloat(currentOperand)
-            break
-    }
+  switch (operator) {
+    case '+':
+      result = firstOperand + Number(currentOperand)
+      break
+    case '-':
+      result = firstOperand - Number(currentOperand)
+      break
+    case '/':
+      if (Number(currentOperand) == 0) {
+        result = 'error'
+      } else {
+        result = firstOperand / Number(currentOperand)
+      }
+      break
+    case '*':
+      result = firstOperand * Number(currentOperand)
+      break
+  }
 
-    return result
+  return result
 }
 
-function formatResult(result) {
-    let resultString = String(result)
-    if (resultString.length > MAX_DISPLAY_DIGIT_LENGTH) {
-        result = result.toExponential(2)
-    }
-    return result
+function formatResult (result) {
+  let resultString = String(result)
+
+  if (resultString.length > MAX_DISPLAY_DIGIT_LENGTH) {
+    result = result.toExponential(2)
+  }
+  return result
 }
 // -----------------------------------------------------------------------MAIN--------------------------------------------------------------
 
-function updateCalculatorStatus(){
-    document.getElementById('calculatorDisplay').value = String(currentOperand).replace('.', ',')
+function updateCalculatorStatus () {
+  let valueToDisplay = currentOperand
 
-    // if(pendingResult){
-    //     calcular(num1,num2)
-    // }
+  if (valueToDisplay == '') {
+    valueToDisplay = 0
+  }
+  document.getElementById('calculatorDisplay').value = String(valueToDisplay).replace('.', ',')
 
-    // if(isNegative){
-    //     if(operatorSelected){
-    //         toggleNegativePositive(num2)
-    //     } else {
-    //         toggleNegativePositive(num1)
-    //     }
-    // }
+  if (currentOperand.length >= MAX_DISPLAY_DIGIT_LENGTH || hasResult) {
+    disableNumericButtons()
+    if (Number(currentOperand) > 0 || hasResult) {
+      disableToggleNegativeButton()
+    }
+    return
+  } else {
+    enableNumericButtons()
+    enableToggleNegativeButton()
+  }
 
-    // if(pendingReset){
-    //     reset()
-    // }
+  if (operator != '') {
+    enableNumericButtons()
+    enableToggleNegativeButton()
+  }
 }
 
+function disableNumberButtons () {
+  domNumberButtons.forEach(button => {
+    button.setAttribute('disabled', '')
+    button.classList.add('disabled-number-buttons')
+  })
+}
+
+function disableCommaButton () {
+  domDecimalButton.setAttribute('disabled', '')
+  domDecimalButton.classList.add('disabled-number-buttons')
+}
+
+function disableZeroButton () {
+  domZeroButton.setAttribute('disabled', '')
+  domZeroButton.classList.add('disabled-number-buttons')
+}
+
+function disableToggleNegativeButton () {
+  domNegativeButton.setAttribute('disabled', '')
+  domNegativeButton.classList.add('disabled-other-buttons')
+}
+
+function enableNumberButtons () {
+  domNumberButtons.forEach(button => {
+    button.removeAttribute('disabled')
+    button.classList.remove('disabled-number-buttons')
+  })
+}
+
+function enableCommaButton () {
+  domDecimalButton.removeAttribute('disabled')
+  domDecimalButton.classList.remove('disabled-number-buttons')
+}
+
+function enableZeroButton () {
+  domZeroButton.removeAttribute('disabled')
+  domZeroButton.classList.remove('disabled-number-buttons')
+}
+
+function enableToggleNegativeButton () {
+  domNegativeButton.removeAttribute('disabled')
+  domNegativeButton.classList.remove('disabled-other-buttons')
+}
+
+function disableNumericButtons () {
+  disableNumberButtons()
+  disableCommaButton()
+  disableZeroButton()
+}
+
+function enableNumericButtons () {
+  enableNumberButtons()
+  enableCommaButton()
+  enableZeroButton()
+}
